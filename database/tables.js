833ae5a5d414f0c6a1db49132e4357125df3fb69
pravin_arzooo <pravin@homeyantra.com>
2020-05-31 16:51:37 +0530

    Add table interactions

--- database/tables.js
+++ database/tables.js
@@ -26,15 +26,73 @@ const createUserTable = () => {
     });
 };
 
-pool.on('remove', () => {
-  console.log('client removed');
-  process.exit(0);
-});
+const updateUserTable = () => {
+  const updateUserQuery = `
+              ALTER TABLE users ADD COLUMN image VARCHAR(255)`;
+  
+  pool.query(updateUserQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+
+
+const createTypes = () => {
+  const typeQuery = `CREATE TYPE operation AS ENUM ('LIKE', 'SUPERLIKE', 'BLOCK')`;
+  pool.query(typeQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+
+const createInteractionTable = () => {
+  const interactionQuery = `CREATE TABLE IF NOT EXISTS interactions
+  (id SERIAL PRIMARY KEY, 
+  base_user INTEGER NOT NULL, 
+  target_user INTEGER NOT NULL, 
+  user_activity operation,
+  created_on DATE DEFAULT CURRENT_TIMESTAMP NOT NULL)`;
+
+  pool.query(interactionQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+
+
+
+
 
 
 const createAllTables = () => {
   createUserTable();
+  updateUserTable();
+  createTypes();
+  createInteractionTable();
 };
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
 
 module.exports =  {
   createAllTables
