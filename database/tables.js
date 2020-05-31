2ba212d687acc43ba5f2376761543b12b3f78009
pravin_arzooo <pravin@homeyantra.com>
2020-06-01 00:32:50 +0530

    Write the script for adding sample users

--- database/tables.js
+++ database/tables.js
@@ -2,9 +2,11 @@
 const dbUtils = require("./sql.js");
 const pool = dbUtils.pool;
 
+
 pool.on('connect', () => {
   console.log('connected to the db');
 });
+
 
 
 const createUserTable = () => {
@@ -76,6 +78,33 @@ const createInteractionTable = () => {
     });
 };
 
+const insertSampleUsers = () => {
+  const usersQuery = `INSERT into users(email, name, password, image) 
+                      values
+                      ('john@gmail.com', 'john', 'password1', 'https://picsum.photos/id/1/200/300'),
+                      ('smith@gmail.com', 'smith', 'password2', 'https://picsum.photos/id/2/200/300'),
+                      ('warner@gmail.com', 'warner', 'password3', 'https://picsum.photos/id/3/200/300'),
+                      ('rohit@gmail.com', 'rohit', 'password4', 'https://picsum.photos/id/4/200/300'),
+                      ('virat@gmail.com', 'virat', 'password5', 'https://picsum.photos/id/5/200/300'),
+                      ('dhoni@gmail.com', 'dhoni', 'password6', 'https://picsum.photos/id/6/200/300'),
+                      ('ponting@gmail.com', 'ponting', 'password7', 'https://picsum.photos/id/7/200/300'),
+                      ('anderson@gmail.com', 'anderson', 'password8', 'https://picsum.photos/id/8/200/300'),
+                      ('steyn@gmail.com', 'steyn', 'password9', 'https://picsum.photos/id/9/200/300'),
+                      ('bravo@gmail.com', 'bravo', 'password10', 'https://picsum.photos/id/10/200/300')`;
+
+  
+  pool.query(usersQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+  });
+                      
+}
+
 
 
 
@@ -87,6 +116,7 @@ const createAllTables = () => {
   updateUserTable();
   createTypes();
   createInteractionTable();
+  insertSampleUsers();
 };
 
 pool.on('remove', () => {
